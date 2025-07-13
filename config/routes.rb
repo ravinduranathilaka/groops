0af9d14f82d1bd86012569a9a9e2dbec4bcd5cd9
Ravindu Ranathilaka <ravindu925@gmail.com>
2025-07-13 13:34:24 +0900

    fixed nav bar and correlated pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users
   resources :groups
   #get "home/index"
-  root "home#index"
+  root "tasks#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
