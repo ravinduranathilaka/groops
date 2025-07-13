618d26a671d2d3353a6ff2942d1a8b5aa011cf2a
Ravindu Ranathilaka <ravindu925@gmail.com>
2025-07-13 19:56:54 +0900

    previous puss forgot to

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,35 @@
 module TasksHelper
-end
+    # Return Tailwind classes based on urgency level
+    def urgency_color(urgency)
+      case urgency.to_s
+      when "urgent"
+        "text-red-600 font-semibold"
+      when "high"
+        "text-orange-500 font-medium"
+      when "mid"
+        "text-yellow-500"
+      when "low"
+        "text-green-600"
+      else
+        "text-gray-500"
+      end
+    end
+  
+    # Return Tailwind classes based on status
+    def status_color(status)
+      case status.to_s
+      when "unassigned"
+        "text-gray-400 italic"
+      when "assigned"
+        "text-blue-500 font-medium"
+      when "started"
+        "text-yellow-600 font-medium"
+      when "completed"
+        "text-green-600 font-semibold"
+      when "cancelled"
+        "text-red-500 line-through"
+      else
+        "text-gray-500"
+      end
+    end
+  end
