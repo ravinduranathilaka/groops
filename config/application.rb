593a69f18bf844bf522ecebb1e4bca8874c88ff0
Ravindu Ranathilaka <ravindu925@gmail.com>
2025-07-15 23:38:30 +0900

    production fixes

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ module Groops
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
-    config.active_record.solid_cache = nil
-    config.active_record.solid_queue = nil
+    #config.active_record.solid_cache = nil
+    #config.active_record.solid_queue = nil
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
