4a308de90504e0b2be7fa496af65ed9ae55a1f6c
Ravindu Ranathilaka <ravindu925@gmail.com>
2025-07-15 22:42:32 +0900

    cable fixes

--- config/initializers/solid_cable.rb
+++ config/initializers/solid_cable.rb
@@ -1,1 +1,5 @@
-SolidCable::Record.connects_to database: { writing: :primary }
+Rails.application.config.after_initialize do
+    if defined?(SolidCable::Record)
+      SolidCable::Record.connects_to database: { writing: :primary }
+    end
+  end
